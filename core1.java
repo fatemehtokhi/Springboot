166938fff7ac95bfa39891a4cb9c50b5c9f2216e
your_github_username <your_github_email>
2022-07-03 11:14:44 +0200

    core1

---
+++ core1.java
@@ -0,0 +1,13 @@
+
+public class core1 {
+    public static void main(String[] args) {
+        int x=2;
+    if(x==2){
+        System.out.println("Ich bin Fatemah");
+
+    }
+    }
+    
+    
+
+}
---
+++ register.java
@@ -0,0 +1,12 @@
+public class register{
+    public static void main(String[] args) {
+        short s = 0;
+        int x = 07;
+        int y = 05;
+        int z = 123456;
+        s += z;
+            
+        System.out.println("" + x + y + s);
+
+    }
+}
